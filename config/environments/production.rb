eb076d3920ca9f657f23ff1e50954be2a631a5d1
Sergey <sergeyskumatov@aol.com>
2016-07-13 16:53:49 -0600

    gmails test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,21 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.default_url_options = { :host => 'limitless-fortress-77954.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "limitless-fortress-77954.herokuapp.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV["GMAIL_USERNAME"],
+      password: ENV["GMAIL_PASSWORD"]
+  }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
@@ -83,17 +98,17 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'limitless-fortress-77954.herokuapp.com' }
 
-  Mail.defaults do
-    delivery_method :smtp, {
-        :address => 'smtp.sendgrid.net',
-        :port => '587',
-        :domain => 'heroku.com',
-        :user_name => ENV['SENDGRID_USERNAME'],
-        :password => ENV['SENDGRID_PASSWORD'],
-        :authentication => :plain,
-        :enable_starttls_auto => true
-    }
-  end
+  # Mail.defaults do
+  #   delivery_method :smtp, {
+  #       :address => 'smtp.sendgrid.net',
+  #       :port => '587',
+  #       :domain => 'heroku.com',
+  #       :user_name => ENV['SENDGRID_USERNAME'],
+  #       :password => ENV['SENDGRID_PASSWORD'],
+  #       :authentication => :plain,
+  #       :enable_starttls_auto => true
+  #   }
+  # end
 
   # Mail.deliver do
   #   to 'sergeyskumatov@aol.com'
