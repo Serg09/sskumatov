8dcf8586ee32d10de089da15a69a0282f2cd98d4
Sergey <sergeyskumatov@aol.com>
2016-07-13 19:07:47 -0600

    mails settings changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,17 +98,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'limitless-fortress-77954.herokuapp.com' }
 
-  # Mail.defaults do
-  #   delivery_method :smtp, {
-  #       :address => 'smtp.sendgrid.net',
-  #       :port => '587',
-  #       :domain => 'heroku.com',
-  #       :user_name => ENV['SENDGRID_USERNAME'],
-  #       :password => ENV['SENDGRID_PASSWORD'],
-  #       :authentication => :plain,
-  #       :enable_starttls_auto => true
-  #   }
-  # end
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
 
   # Mail.deliver do
   #   to 'sergeyskumatov@aol.com'
