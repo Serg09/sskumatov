e95ccd5e1c53b245e16ba69df1c4e32c9c7c2508
Sergey <sergeyskumatov@aol.com>
2016-07-30 21:09:36 -0600

    few changes to the views to make them work correctly

--- app/controllers/secret_controller.rb
+++ app/controllers/secret_controller.rb
@@ -3,3 +3,27 @@ class SecretController < ApplicationController
     before_action :authenticate_user!, only: [:secret]
   end
 end
+
+
+# this action will return to the same page after log in
+
+#
+# class SecretController < ApplicationController
+#
+#   # before_filter :store_current_location, :unless => :devise_controller?
+#   before_filter :store_current_location, :unless => :devise_controller?
+#
+#   def index
+#     before_action :authenticate_user!, only: [:secret]
+#   end
+#
+#
+#   private
+#   def store_current_location
+#     store_location_for(:user, request.url)
+#   end
+#
+#   def after_sign_out_path_for(resource)
+#     request.referrer || root_path
+#   end
+# end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,9 +3,15 @@ class StaticPagesController < ApplicationController
   end
 
   def about
+    # if user_signed_in?
+    #   redirect_to :about
+    # else
+    #   redirect_to static_pages_home_path
+    # end
   end
 
   def contact
+    # if user_signed_in?
     # redirect_to :back
   end
 end
