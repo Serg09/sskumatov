ec2faaa593eab1b0dcbbd3f6d1083a8c95cb611a
Serg09 <sergeyskumatov@aol.com>
2016-09-21 14:26:02 -0600

    another sendgrid change for heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,27 +49,14 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.default_options = {from: 'sergeyskumatov@gmail.com'}
 
-  # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
-  #     address: "smtp.gmail.com",
-  #     port: 587,
-  #     domain: "gmail.com",
-  #     authentication: "plain",
-  #     enable_starttls_auto: true,
-  #     user_name: ENV["GMAIL_USERNAME"],
-  #     password: ENV["GMAIL_PASSWORD"],
-  #     openssl_verify_mode: 'none'
+  #     :address        => 'smtp.sendgrid.net',
+  #     :port           => '587',
+  #     :authentication => :plain,
+  #     :user_name      => ENV['SENDGRID_USERNAME_DEV'],
+  #     :password       => ENV['SENDGRID_PASSWORD_DEV'],
+  #     :domain         => 'heroku.com',
+  #     :enable_starttls_auto => true
   # }
-
-  # ActionMailer::Base.smtp_settings = {
-  config.action_mailer.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME_DEV'],
-      :password       => ENV['SENDGRID_PASSWORD_DEV'],
-      :domain         => 'heroku.com',
-      :enable_starttls_auto => true
-  }
 
 end
