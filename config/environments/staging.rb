25eba85bb50ef6e3549bd1045ab56017fef9bc86
Sergey Skumatov <sergeyskumatov@aol.com>
2018-11-27 21:02:52 -0700

    change heroku debug back to original settings

--- config/environments/staging.rb
+++
@@ -1,7 +0,0 @@
-# staging.rb, production.rb
-
-# Log detail is configurable on the server
-config.log_level = ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].to_sym : ('info').to_sym
-
-#staging.rb
-config.consider_all_requests_local = ENV['CONSIDER_REQUESTS_LOCAL']
